fcb7c46ac41200c558b74658b45312019324a31c
nox <noxhsxrk@icloud.com>
2024-11-07 12:02:37 +0700

    fix: update ai-pre-commit version in sha256 hash

--- ai-pre-commit.rb
+++ ai-pre-commit.rb
@@ -2,7 +2,7 @@ class AiPreCommit < Formula
     desc "A description of your project"
     homepage "https://github.com/noxhsxrk/ai-pre-commit"
     url "https://github.com/noxhsxrk/ai-pre-commit/archive/refs/tags/v1.0.1.tar.gz"
-    sha256 "ec7ef4b9871458b6f917bbc395727d4c2197f4b042eae3e43e5a26de6f1aa88d"
+    sha256 "708b8e1f7879808ee893ad7ca22e65e0772c6ef1013e432d66c22cf62d68011b"
     license "MIT"
   
     depends_on "python@3.9"
